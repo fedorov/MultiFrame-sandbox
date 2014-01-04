66d5054682a3a833637458f5dd10d4e96037d1db
Andrey Fedorov <fedorov@bwh.harvard.edu>
2014-01-04 12:06:34 -0500

    ENH: fixed remaining issues for the BWH QIN T2 sequence test

--- dcmultiFixup.cxx
+++ dcmultiFixup.cxx
@@ -35,9 +35,9 @@ int main(int argc, char** argv)
 		  meta->putAndInsertString(DCM_MediaStorageSOPClassUID, "1.2.840.10008.5.1.4.1.1.4.4");
 				// UID_LegacyConvertedEnhancedMRImageStorage);
 		}
+
+    meta->putAndInsertString(DCM_ImplementationClassUID, "1.2.276.0.7230010.3.0.3.6.1");
 	
-		dcm->putAndInsertString(DCM_ImplementationClassUID, 
-				QIICRTestImplementationClassUID);
 		dcm->putAndInsertString(DCM_InstanceCreatorUID, 
 				QIICRTestInstanceCreatorUID);
 		
@@ -49,21 +49,29 @@ int main(int argc, char** argv)
 		dcm->putAndInsertString(DCM_SOPInstanceUID, uidStr.c_str());
 		meta->putAndInsertString(DCM_MediaStorageSOPInstanceUID, uidStr.c_str());
 
+		//uid.generate();
+		//uid.toString(uidStr, OFUUID::ER_RepresentationOID);
+		//dcm->putAndInsertString(DCM_StudyInstanceUID, uidStr.c_str());
+
+		DcmItem *seq, *item, *item2, *item3;
+    // can't find the reason for this sequence existence!
+    // -->
+		status = dcm->findAndGetSequenceItem(DCM_ReferencedRawDataSequence, item);
+    uid.generate();
+		uid.toString(uidStr, OFUUID::ER_RepresentationOID);
+		item->putAndInsertString(DCM_StudyInstanceUID, uidStr.c_str());
+
+		item->findAndGetSequenceItem(DCM_ReferencedSeriesSequence, item2);
 		uid.generate();
 		uid.toString(uidStr, OFUUID::ER_RepresentationOID);
-		dcm->putAndInsertString(DCM_StudyInstanceUID, uidStr.c_str());
+		item2->putAndInsertString(DCM_SeriesInstanceUID, uidStr.c_str());
 
-		uid.generate();
+		item2->findAndGetSequenceItem(DCM_ReferencedSOPSequence, item3);
+    uid.generate();
 		uid.toString(uidStr, OFUUID::ER_RepresentationOID);
-		dcm->putAndInsertString(DCM_SeriesInstanceUID, uidStr.c_str());
+		item3->putAndInsertString(DCM_ReferencedSOPInstanceUID, uidStr.c_str());
+    // <--
 
-		uid.generate();
-		uid.toString(uidStr, OFUUID::ER_RepresentationOID);
-		dcm->putAndInsertString(DCM_FrameOfReferenceUID, uidStr.c_str());
-
-		dcm->findAndDeleteElement(DCM_ReferencedRawDataSequence);
-
-		DcmItem *seq, *item;
 		status = dcm->findAndGetSequenceItem(DCM_DimensionOrganizationSequence, item);
     if(status.good()){
   		uid.generate();
@@ -75,6 +83,22 @@ int main(int argc, char** argv)
         if(status.good())
     		  item->putAndInsertString(DCM_DimensionOrganizationUID, uidStr.c_str());
   		}
+    }
+
+    // initialize AnatomicRegionSequence in the shared attributes
+    dcm->findOrCreateSequenceItem(DCM_SharedFunctionalGroupsSequence, item);
+    item->findOrCreateSequenceItem(DCM_FrameAnatomySequence, item2);
+    item2->findOrCreateSequenceItem(DCM_AnatomicRegionSequence, item3);
+    item3->putAndInsertString(DCM_CodeValue, "T-9200B");
+    item3->putAndInsertString(DCM_CodingSchemeDesignator, "SRT");
+    item3->putAndInsertString(DCM_CodeMeaning, "Prostate");
+
+    // make sure PulseSequenceName is not longer than 16 characters
+    OFString pulseSequenceName;
+    status = dcm->findAndGetOFString(DCM_PulseSequenceName, pulseSequenceName);
+    if(status.good()){
+      pulseSequenceName[16] = '\0';
+      dcm->putAndInsertString(DCM_PulseSequenceName, pulseSequenceName.c_str());
     }
 
 		status = fileformat.saveFile(outputFileName.c_str(), EXS_LittleEndianExplicit);
